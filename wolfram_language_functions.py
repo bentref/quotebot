d231732b0e2041cf0771f5874dbdae009e4a3fae
Rhea Kommerell <rhea.kommerell@gmail.com>
2020-12-25 19:18:25 -0800

    first draft of prediction

--- wolfram_language_functions.py
+++ wolfram_language_functions.py
@@ -1,5 +1,6 @@
 from wolframclient.evaluation import WolframLanguageSession
-from wolframclient.language import wl
+from wolframclient.language import wl, Global, wlexpr
+import re
 session = WolframLanguageSession()
 
 
@@ -8,3 +9,15 @@ def bad_vibe(message):
         return True
     else:
         return False
+
+
+def predict_next_word(database, user, phrase):
+    messages_list = []
+    phrase_list = re.findall(r'\w+', phrase)
+    return session.evaluate(wl.SequencePredict(messages_list)(phrase_list))
+
+
+# def predict_finished_phrase(database, user, phrase, length_remaining):
+#     for i in range(length_remaining):
+#         phrase.append(predict_next_word(database, user, phrase))
+#     return " ".join(phrase)
