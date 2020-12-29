96243112cc4c7dc9ca5dfa38b97b54b632dc1adf
Rhea Kommerell <rhea.kommerell@gmail.com>
2020-12-28 17:14:39 -0800

    sorting list of predictions

--- wolfram_language_functions.py
+++ wolfram_language_functions.py
@@ -10,11 +10,12 @@ def bad_vibe(message):
         return False
 
 
-def predict_next_word(database, user, phrase, probabilities=False):
+def predict_next_word(database, user, phrase, probabilities=False, num_probs=3):
     messages_list = [wl.TextWords(message) for message in database[user]]
     phrase_list = wl.TextWords(phrase)
     if probabilities:
-        return session.evaluate(wl.SequencePredict(messages_list)(phrase_list, "Probabilities"))
+        predictions = wl.SequencePredict(messages_list)(phrase_list, "Probabilities")
+        return session.evaluate(wl.Take(wl.Reverse(wl.Sort(predictions)), num_probs))
     else:
         return session.evaluate(wl.SequencePredict(messages_list)(phrase_list))
 
